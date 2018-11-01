a26bec1aca4a62c60ab9956860f55e4b7c85e28f
Harshal <mail@harshal.xyz>
2018-11-01 13:38:31 +1100

    initial commit

---
+++ LearnXamarin/LearnXamarin/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using LearnXamarin.Views;
+using Xamarin.Forms;
+
+namespace LearnXamarin.ViewModels
+{
+    public class MainPageViewModel
+    {
+        public Command ShowXkcdCommand { get; }
+
+        public MainPageViewModel()
+        {
+            ShowXkcdCommand = new Command(ShowXkcdPage);
+        }
+
+        public string Message { get; set; } = "Hello World";
+
+        private void ShowXkcdPage()
+        {
+            Application.Current.MainPage.Navigation.PushAsync(new XkcdPage());
+        }
+    }
+}
